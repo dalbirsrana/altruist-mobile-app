7a31efdde7e957789626d7250b9c0c2ca5bccdf8
jashankaur <jashanjot177@gmail.com>
2020-10-25 00:42:07 -0700

    UI changes

---
+++ src/colors/colors.js
@@ -0,0 +1,6 @@
+export default {
+  primary: "#E89B8D",
+  secondary: "#FFE7C2",
+  black: "#444142",
+  white: "#FFFFFF",
+};
--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,49 +1,44 @@
-import React, { useContext } from 'react'
-import { Image, Button, StyleSheet, Text, View } from 'react-native'
-import FormButton from '../../common/FormButton'
-import { AuthContext } from '../navigation/AuthProvider'
+import React, { useContext } from "react";
+import { Image, Button, StyleSheet, Text, View } from "react-native";
+import FormButton from "../../common/FormButton";
+import { AuthContext } from "../navigation/AuthProvider";
 
-import logo from '../../../assets/icon.png'
+import logo from "../../../assets/icon.png";
+import colors from "../../colors/colors";
 
 const HomeScreen = ({ navigation }) => {
-    
-    const { user, logout } = useContext(AuthContext)
+  const { user, logout } = useContext(AuthContext);
 
-    return (
-        <View style={styles.container}>
-                <Image source={logo} style={{width: 300, height: 80}} />
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={{ width: 300, height: 80 }} />
 
-                <Text>Welcome { user.name } </Text>
+      <Text>Welcome {user.name} </Text>
 
-                <FormButton buttonTitle='LogOut' onPress={() => logout()} />
-    
-                <Button 
-                title="Open Drawer"
-                onPress={() => navigation.navigate('Test')}
-                />
-    
-                <Text style={styles.para}>
-                    This is home page
-                </Text>
-        </View>
-    )
-} 
+      <FormButton buttonTitle="LogOut" onPress={() => logout()} />
 
-export default HomeScreen
+      <Button title="Open Drawer" onPress={() => navigation.navigate("Test")} />
+
+      <Text style={styles.para}>This is home page</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#ddd',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    para: {
-        fontSize: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
-        color: 'red',
-        textAlign: 'center',
-        marginVertical: 30,
-    }
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  para: {
+    fontSize: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    color: "red",
+    textAlign: "center",
+    marginVertical: 30,
+  },
 });
